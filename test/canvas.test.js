2851b4d7da9d0ff3f7a7b589ee8032b881f7c2ba
Tj Holowaychuk <tj@vision-media.ca>
2010-09-30 15:14:43 -0700

    Added radial grad test

--- test/canvas.test.js
+++ test/canvas.test.js
@@ -388,5 +388,48 @@ module.exports = {
       , path
       , '55f80d7d33000904ca366e0cfe363079'
       , 'Context2d#createLinearGradient() failed');
+  },
+  
+  'test Context2d#createRadialGradient()': function(assert){
+    var canvas = new Canvas(200, 200)
+      , ctx = canvas.getContext('2d')
+      , path = __dirname + '/images/radialGradients.png';
+    
+    // Create gradients
+    var radgrad = ctx.createRadialGradient(45,45,10,52,50,30);
+    radgrad.addColorStop(0, '#A7D30C');
+    radgrad.addColorStop(0.9, '#019F62');
+    radgrad.addColorStop(1, 'rgba(1,159,98,0)');
+
+    var radgrad2 = ctx.createRadialGradient(105,105,20,112,120,50);
+    radgrad2.addColorStop(0, '#FF5F98');
+    radgrad2.addColorStop(0.75, '#FF0188');
+    radgrad2.addColorStop(1, 'rgba(255,1,136,0)');
+
+    var radgrad3 = ctx.createRadialGradient(95,15,15,102,20,40);
+    radgrad3.addColorStop(0, '#00C9FF');
+    radgrad3.addColorStop(0.8, '#00B5E2');
+    radgrad3.addColorStop(1, 'rgba(0,201,255,0)');
+
+    var radgrad4 = ctx.createRadialGradient(0,150,50,0,140,90);
+    radgrad4.addColorStop(0, '#F4F201');
+    radgrad4.addColorStop(0.8, '#E4C700');
+    radgrad4.addColorStop(1, 'rgba(228,199,0,0)');
+
+    // draw shapes
+    ctx.fillStyle = radgrad4;
+    ctx.fillRect(0,0,150,150);
+    ctx.fillStyle = radgrad3;
+    ctx.fillRect(0,0,150,150);
+    ctx.fillStyle = radgrad2;
+    ctx.fillRect(0,0,150,150);
+    ctx.fillStyle = radgrad;
+    ctx.fillRect(0,0,150,150);
+
+    assertChecksum(
+        canvas
+      , path
+      , 'd078f5993eb962a5b3fdde5ca0864179'
+      , 'Context2d#createRadialGradient() failed');
   }
 }
