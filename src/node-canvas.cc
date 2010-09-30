f43ae9252c416157bb8d3c7774053ea30bd8934e
Tj Holowaychuk <tj@vision-media.ca>
2010-09-30 12:42:21 -0700

    Exposing cairo version. Closes #1

--- src/node-canvas.cc
+++ src/node-canvas.cc
@@ -13,4 +13,5 @@ init (Handle<Object> target) {
   HandleScope scope;
   Canvas::Initialize(target);
   Context2d::Initialize(target);
+  target->Set(String::New("cairoVersion"), String::New(cairo_version_string()));
 }
