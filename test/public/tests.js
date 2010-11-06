e5a3b86ed0e3e76aace42ddfbdb4628fc1be6c7c
Tj Holowaychuk <tj@vision-media.ca>
2010-11-05 17:24:26 -0700

    Added another arc() test from MDC

--- test/public/tests.js
+++ test/public/tests.js
@@ -42,6 +42,28 @@ tests['arc()'] = function(ctx){
   ctx.stroke();
 };
 
+tests['arc() 2'] = function(ctx){
+  for(var i=0;i<4;i++){
+    for(var j=0;j<3;j++){
+      ctx.beginPath();
+      var x              = 25+j*50;               // x coordinate
+      var y              = 25+i*50;               // y coordinate
+      var radius         = 20;                    // Arc radius
+      var startAngle     = 0;                     // Starting point on circle
+      var endAngle       = Math.PI+(Math.PI*j)/2; // End point on circle
+      var anticlockwise  = i%2==0 ? false : true; // clockwise or anticlockwise
+
+      ctx.arc(x,y,radius,startAngle,endAngle, anticlockwise);
+
+      if (i>1){
+        ctx.fill();
+      } else {
+        ctx.stroke();
+      }
+    }
+  }
+};
+
 tests['bezierCurveTo()'] = function(ctx){
   ctx.beginPath();
   ctx.moveTo(75,40);
