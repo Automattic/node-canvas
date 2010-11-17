78fd2759a2a0197cff9d55a9b8ab10f35b9f5ee6
Tj Holowaychuk <tj@vision-media.ca>
2010-11-17 10:55:20 -0800

    Added more putImageData() tests

--- test/public/tests.js
+++ test/public/tests.js
@@ -1532,7 +1532,7 @@ tests['putImageData() 6'] = function(ctx){
   ctx.putImageData(data,60,60,10,0,35,30);
 };
 
-tests['putImageData() CTM'] = function(ctx){
+tests['putImageData() 7'] = function(ctx){
   for (i=0;i<6;i++){
     for (j=0;j<6;j++){
       ctx.fillStyle = 'rgb(' + Math.floor(255-42.5*i) + ',' + 
@@ -1543,7 +1543,21 @@ tests['putImageData() CTM'] = function(ctx){
   ctx.strokeRect(60,60,50,30);
   ctx.translate(20,20);
   var data = ctx.getImageData(0,0,50,50);
-  ctx.putImageData(data,60,60,10,20,35,30);
+  ctx.putImageData(data,60,60,10,20,35,-10);
+};
+
+tests['putImageData() 8'] = function(ctx){
+  for (i=0;i<6;i++){
+    for (j=0;j<6;j++){
+      ctx.fillStyle = 'rgb(' + Math.floor(255-42.5*i) + ',' + 
+                       Math.floor(255-42.5*j) + ',0)';
+      ctx.fillRect(j*25,i*25,25,25);
+    }
+  }
+  ctx.strokeRect(60,60,50,30);
+  ctx.translate(20,20);
+  var data = ctx.getImageData(0,0,50,50);
+  ctx.putImageData(data,-10,-10,0,20,35,30);
 };
 
 tests['putImageData() alpha'] = function(ctx){
