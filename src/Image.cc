762f8865de244829ba9bb5568e9aa26b0b34307f
Tj Holowaychuk <tj@vision-media.ca>
2012-01-04 10:48:52 -0800

    misc cleanup

--- src/Image.cc
+++ src/Image.cc
@@ -553,23 +553,28 @@ Image::loadGIFFromBuffer(uint8_t *buf, unsigned len) {
 #if JPEG_LIB_VERSION < 80
 
 /* Read JPEG image from a memory segment */
-static void init_source (j_decompress_ptr cinfo) {}
-static boolean fill_input_buffer (j_decompress_ptr cinfo) {
+static void
+init_source(j_decompress_ptr cinfo) {}
+
+static boolean
+fill_input_buffer(j_decompress_ptr cinfo) {
   ERREXIT(cinfo, JERR_INPUT_EMPTY);
   return TRUE;
 }
-static void skip_input_data (j_decompress_ptr cinfo, long num_bytes) {
+static void
+skip_input_data(j_decompress_ptr cinfo, long num_bytes) {
   struct jpeg_source_mgr* src = (struct jpeg_source_mgr*) cinfo->src;
   if (num_bytes > 0) {
     src->next_input_byte += (size_t) num_bytes;
     src->bytes_in_buffer -= (size_t) num_bytes;
   }
 }
+
 static void term_source (j_decompress_ptr cinfo) {}
 static void jpeg_mem_src (j_decompress_ptr cinfo, void* buffer, long nbytes) {
   struct jpeg_source_mgr* src;
 
-  if (cinfo->src == NULL) {   /* first time for this JPEG object? */
+  if (cinfo->src == NULL) {
     cinfo->src = (struct jpeg_source_mgr *)
       (*cinfo->mem->alloc_small) ((j_common_ptr) cinfo, JPOOL_PERMANENT,
                                   sizeof(struct jpeg_source_mgr));
