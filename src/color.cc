6f90054ede3c3a27b3300e38a830ebd3aba83a6a
obarthel <obarthel@gmx.net>
2012-01-30 10:19:25 +0100

    Named color table rebuilt from CSS Color Module Level 3 specification

--- src/color.cc
+++ src/color.cc
@@ -41,151 +41,159 @@ static struct named_color {
   const char *name;
   uint32_t val;
 } named_colors[] = {
-    { "transparent", 0xffffff00}
-  , { "aliceblue", 0xf0f8ffff }
-  , { "antiquewhite", 0xfaebd7ff }
-  , { "aqua", 0x00ffffff }
-  , { "aquamarine", 0x7fffd4ff }
-  , { "azure", 0xf0ffffff }
-  , { "beige", 0xf5f5dcff }
-  , { "bisque", 0xffe4c4ff }
-  , { "black", 0x000000ff }
-  , { "blanchedalmond", 0xffebcdff }
-  , { "blue", 0x0000ffff }
-  , { "blueviolet", 0x8a2be2ff }
-  , { "brown", 0xa52a2aff }
-  , { "burlywood", 0xdeb887ff }
-  , { "cadetblue", 0x5f9ea0ff }
-  , { "chartreuse", 0x7fff00ff }
-  , { "chocolate", 0xd2691eff }
-  , { "coral", 0xff7f50ff }
-  , { "cornflowerblue", 0x6495edff }
-  , { "cornsilk", 0xfff8dcff }
-  , { "crimson", 0xdc143cff }
-  , { "cyan", 0x00ffffff }
-  , { "darkblue", 0x00008bff }
-  , { "darkcyan", 0x008b8bff }
-  , { "darkgoldenrod", 0xb8860bff }
-  , { "darkgray", 0xa9a9a9ff }
-  , { "darkgreen", 0x006400ff }
-  , { "darkkhaki", 0xbdb76bff }
-  , { "darkmagenta", 0x8b008bff }
-  , { "darkolivegreen", 0x556b2fff }
-  , { "darkorange", 0xff8c00ff }
-  , { "darkorchid", 0x9932ccff }
-  , { "darkred", 0x8b0000ff }
-  , { "darksalmon", 0xe9967aff }
-  , { "darkseagreen", 0x8fbc8fff }
-  , { "darkslateblue", 0x483d8bff }
-  , { "darkslategray", 0x2f4f4fff }
-  , { "darkturquoise", 0x00ced1ff }
-  , { "darkviolet", 0x9400d3ff }
-  , { "deeppink", 0xff1493ff }
-  , { "deepskyblue", 0x00bfffff }
-  , { "dimgray", 0x696969ff }
-  , { "dodgerblue", 0x1e90ffff }
-  , { "feldspar", 0xd19275ff }
-  , { "firebrick", 0xb22222ff }
-  , { "floralwhite", 0xfffaf0ff }
-  , { "forestgreen", 0x228b22ff }
-  , { "fuchsia", 0xff00ffff }
-  , { "gainsboro", 0xdcdcdcff }
-  , { "ghostwhite", 0xf8f8ffff }
-  , { "gold", 0xffd700ff }
-  , { "goldenrod", 0xdaa520ff }
-  , { "gray", 0x808080ff }
-  , { "green", 0x008000ff }
-  , { "greenyellow", 0xadff2fff }
-  , { "honeydew", 0xf0fff0ff }
-  , { "hotpink", 0xff69b4ff }
-  , { "indianred ", 0xcd5c5cff }
-  , { "indigo ", 0x4b0082ff }
-  , { "ivory", 0xfffff0ff }
-  , { "khaki", 0xf0e68cff }
-  , { "lavender", 0xe6e6faff }
-  , { "lavenderblush", 0xfff0f5ff }
-  , { "lawngreen", 0x7cfc00ff }
-  , { "lemonchiffon", 0xfffacdff }
-  , { "lightblue", 0xadd8e6ff }
-  , { "lightcoral", 0xf08080ff }
-  , { "lightcyan", 0xe0ffffff }
-  , { "lightgoldenrodyellow", 0xfafad2ff }
-  , { "lightgrey", 0xd3d3d3ff }
-  , { "lightgreen", 0x90ee90ff }
-  , { "lightpink", 0xffb6c1ff }
-  , { "lightsalmon", 0xffa07aff }
-  , { "lightseagreen", 0x20b2aaff }
-  , { "lightskyblue", 0x87cefaff }
-  , { "lightslateblue", 0x8470ffff }
-  , { "lightslategray", 0x778899ff }
-  , { "lightsteelblue", 0xb0c4deff }
-  , { "lightyellow", 0xffffe0ff }
-  , { "lime", 0x00ff00ff }
-  , { "limegreen", 0x32cd32ff }
-  , { "linen", 0xfaf0e6ff }
-  , { "magenta", 0xff00ffff }
-  , { "maroon", 0x800000ff }
-  , { "mediumaquamarine", 0x66cdaaff }
-  , { "mediumblue", 0x0000cdff }
-  , { "mediumorchid", 0xba55d3ff }
-  , { "mediumpurple", 0x9370d8ff }
-  , { "mediumseagreen", 0x3cb371ff }
-  , { "mediumslateblue", 0x7b68eeff }
-  , { "mediumspringgreen", 0x00fa9aff }
-  , { "mediumturquoise", 0x48d1ccff }
-  , { "mediumvioletred", 0xc71585ff }
-  , { "midnightblue", 0x191970ff }
-  , { "mintcream", 0xf5fffaff }
-  , { "mistyrose", 0xffe4e1ff }
-  , { "moccasin", 0xffe4b5ff }
-  , { "navajowhite", 0xffdeadff }
-  , { "navy", 0x000080ff }
-  , { "oldlace", 0xfdf5e6ff }
-  , { "olive", 0x808000ff }
-  , { "olivedrab", 0x6b8e23ff }
-  , { "orange", 0xffa500ff }
-  , { "orangered", 0xff4500ff }
-  , { "orchid", 0xda70d6ff }
-  , { "palegoldenrod", 0xeee8aaff }
-  , { "palegreen", 0x98fb98ff }
-  , { "paleturquoise", 0xafeeeeff }
-  , { "palevioletred", 0xd87093ff }
-  , { "papayawhip", 0xffefd5ff }
-  , { "peachpuff", 0xffdab9ff }
-  , { "peru", 0xcd853fff }
-  , { "pink", 0xffc0cbff }
-  , { "plum", 0xdda0ddff }
-  , { "powderblue", 0xb0e0e6ff }
-  , { "purple", 0x800080ff }
-  , { "red", 0xff0000ff }
-  , { "rosybrown", 0xbc8f8fff }
-  , { "royalblue", 0x4169e1ff }
-  , { "saddlebrown", 0x8b4513ff }
-  , { "salmon", 0xfa8072ff }
-  , { "sandybrown", 0xf4a460ff }
-  , { "seagreen", 0x2e8b57ff }
-  , { "seashell", 0xfff5eeff }
-  , { "sienna", 0xa0522dff }
-  , { "silver", 0xc0c0c0ff }
-  , { "skyblue", 0x87ceebff }
-  , { "slateblue", 0x6a5acdff }
-  , { "slategray", 0x708090ff }
-  , { "snow", 0xfffafaff }
-  , { "springgreen", 0x00ff7fff }
-  , { "steelblue", 0x4682b4ff }
-  , { "tan", 0xd2b48cff }
-  , { "teal", 0x008080ff }
-  , { "thistle", 0xd8bfd8ff }
-  , { "tomato", 0xff6347ff }
-  , { "turquoise", 0x40e0d0ff }
-  , { "violet", 0xee82eeff }
-  , { "violetred", 0xd02090ff }
-  , { "wheat", 0xf5deb3ff }
-  , { "white", 0xffffffff }
-  , { "whitesmoke", 0xf5f5f5ff }
-  , { "yellow", 0xffff00ff }
-  , { "yellowgreen", 0x9acd32ff }
-  , { NULL, NULL }
+/*
+ * Olaf (2011-05-31): rebuilt from CSS Color Module Level 3 specification,
+ * section 4.3 (http://www.w3.org/TR/css3-color/).
+ */
+{ "transparent", 0xFFFFFF00},
+{ "aliceblue", 0xF0F8FFFF },
+{ "antiquewhite", 0xFAEBD7FF },
+{ "aqua", 0x00FFFFFF },
+{ "aquamarine", 0x7FFFD4FF },
+{ "azure", 0xF0FFFFFF },
+{ "beige", 0xF5F5DCFF },
+{ "bisque", 0xFFE4C4FF },
+{ "black", 0x000000FF },
+{ "blanchedalmond", 0xFFEBCDFF },
+{ "blue", 0x0000FFFF },
+{ "blueviolet", 0x8A2BE2FF },
+{ "brown", 0xA52A2AFF },
+{ "burlywood", 0xDEB887FF },
+{ "cadetblue", 0x5F9EA0FF },
+{ "chartreuse", 0x7FFF00FF },
+{ "chocolate", 0xD2691EFF },
+{ "coral", 0xFF7F50FF },
+{ "cornflowerblue", 0x6495EDFF },
+{ "cornsilk", 0xFFF8DCFF },
+{ "crimson", 0xDC143CFF },
+{ "cyan", 0x00FFFFFF },
+{ "darkblue", 0x00008BFF },
+{ "darkcyan", 0x008B8BFF },
+{ "darkgoldenrod", 0xB8860BFF },
+{ "darkgray", 0xA9A9A9FF },
+{ "darkgreen", 0x006400FF },
+{ "darkgrey", 0xA9A9A9FF },
+{ "darkkhaki", 0xBDB76BFF },
+{ "darkmagenta", 0x8B008BFF },
+{ "darkolivegreen", 0x556B2FFF },
+{ "darkorange", 0xFF8C00FF },
+{ "darkorchid", 0x9932CCFF },
+{ "darkred", 0x8B0000FF },
+{ "darksalmon", 0xE9967AFF },
+{ "darkseagreen", 0x8FBC8FFF },
+{ "darkslateblue", 0x483D8BFF },
+{ "darkslategray", 0x2F4F4FFF },
+{ "darkslategrey", 0x2F4F4FFF },
+{ "darkturquoise", 0x00CED1FF },
+{ "darkviolet", 0x9400D3FF },
+{ "deeppink", 0xFF1493FF },
+{ "deepskyblue", 0x00BFFFFF },
+{ "dimgray", 0x696969FF },
+{ "dimgrey", 0x696969FF },
+{ "dodgerblue", 0x1E90FFFF },
+{ "firebrick", 0xB22222FF },
+{ "floralwhite", 0xFFFAF0FF },
+{ "forestgreen", 0x228B22FF },
+{ "fuchsia", 0xFF00FFFF },
+{ "gainsboro", 0xDCDCDCFF },
+{ "ghostwhite", 0xF8F8FFFF },
+{ "gold", 0xFFD700FF },
+{ "goldenrod", 0xDAA520FF },
+{ "gray", 0x808080FF },
+{ "green", 0x008000FF },
+{ "greenyellow", 0xADFF2FFF },
+{ "grey", 0x808080FF },
+{ "honeydew", 0xF0FFF0FF },
+{ "hotpink", 0xFF69B4FF },
+{ "indianred", 0xCD5C5CFF },
+{ "indigo", 0x4B0082FF },
+{ "ivory", 0xFFFFF0FF },
+{ "khaki", 0xF0E68CFF },
+{ "lavender", 0xE6E6FAFF },
+{ "lavenderblush", 0xFFF0F5FF },
+{ "lawngreen", 0x7CFC00FF },
+{ "lemonchiffon", 0xFFFACDFF },
+{ "lightblue", 0xADD8E6FF },
+{ "lightcoral", 0xF08080FF },
+{ "lightcyan", 0xE0FFFFFF },
+{ "lightgoldenrodyellow", 0xFAFAD2FF },
+{ "lightgray", 0xD3D3D3FF },
+{ "lightgreen", 0x90EE90FF },
+{ "lightgrey", 0xD3D3D3FF },
+{ "lightpink", 0xFFB6C1FF },
+{ "lightsalmon", 0xFFA07AFF },
+{ "lightseagreen", 0x20B2AAFF },
+{ "lightskyblue", 0x87CEFAFF },
+{ "lightslategray", 0x778899FF },
+{ "lightslategrey", 0x778899FF },
+{ "lightsteelblue", 0xB0C4DEFF },
+{ "lightyellow", 0xFFFFE0FF },
+{ "lime", 0x00FF00FF },
+{ "limegreen", 0x32CD32FF },
+{ "linen", 0xFAF0E6FF },
+{ "magenta", 0xFF00FFFF },
+{ "maroon", 0x800000FF },
+{ "mediumaquamarine", 0x66CDAAFF },
+{ "mediumblue", 0x0000CDFF },
+{ "mediumorchid", 0xBA55D3FF },
+{ "mediumpurple", 0x9370DBFF },
+{ "mediumseagreen", 0x3CB371FF },
+{ "mediumslateblue", 0x7B68EEFF },
+{ "mediumspringgreen", 0x00FA9AFF },
+{ "mediumturquoise", 0x48D1CCFF },
+{ "mediumvioletred", 0xC71585FF },
+{ "midnightblue", 0x191970FF },
+{ "mintcream", 0xF5FFFAFF },
+{ "mistyrose", 0xFFE4E1FF },
+{ "moccasin", 0xFFE4B5FF },
+{ "navajowhite", 0xFFDEADFF },
+{ "navy", 0x000080FF },
+{ "oldlace", 0xFDF5E6FF },
+{ "olive", 0x808000FF },
+{ "olivedrab", 0x6B8E23FF },
+{ "orange", 0xFFA500FF },
+{ "orangered", 0xFF4500FF },
+{ "orchid", 0xDA70D6FF },
+{ "palegoldenrod", 0xEEE8AAFF },
+{ "palegreen", 0x98FB98FF },
+{ "paleturquoise", 0xAFEEEEFF },
+{ "palevioletred", 0xDB7093FF },
+{ "papayawhip", 0xFFEFD5FF },
+{ "peachpuff", 0xFFDAB9FF },
+{ "peru", 0xCD853FFF },
+{ "pink", 0xFFC0CBFF },
+{ "plum", 0xDDA0DDFF },
+{ "powderblue", 0xB0E0E6FF },
+{ "purple", 0x800080FF },
+{ "red", 0xFF0000FF },
+{ "rosybrown", 0xBC8F8FFF },
+{ "royalblue", 0x4169E1FF },
+{ "saddlebrown", 0x8B4513FF },
+{ "salmon", 0xFA8072FF },
+{ "sandybrown", 0xF4A460FF },
+{ "seagreen", 0x2E8B57FF },
+{ "seashell", 0xFFF5EEFF },
+{ "sienna", 0xA0522DFF },
+{ "silver", 0xC0C0C0FF },
+{ "skyblue", 0x87CEEBFF },
+{ "slateblue", 0x6A5ACDFF },
+{ "slategray", 0x708090FF },
+{ "slategrey", 0x708090FF },
+{ "snow", 0xFFFAFAFF },
+{ "springgreen", 0x00FF7FFF },
+{ "steelblue", 0x4682B4FF },
+{ "tan", 0xD2B48CFF },
+{ "teal", 0x008080FF },
+{ "thistle", 0xD8BFD8FF },
+{ "tomato", 0xFF6347FF },
+{ "turquoise", 0x40E0D0FF },
+{ "violet", 0xEE82EEFF },
+{ "wheat", 0xF5DEB3FF },
+{ "white", 0xFFFFFFFF },
+{ "whitesmoke", 0xF5F5F5FF },
+{ "yellow", 0xFFFF00FF },
+{ "yellowgreen", 0x9ACD32FF },
+{ NULL, 0 }
 };
 
 /*
