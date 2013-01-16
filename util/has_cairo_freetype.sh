16ae06f1dce7e9c5a3337f43fe496ad9c6ced59e
TJ Holowaychuk <tj@vision-media.ca>
2013-01-16 13:27:47 -0800

    refactor has_cairo_freetype.sh

--- util/has_cairo_freetype.sh
+++ util/has_cairo_freetype.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
-has_cairo_freetyep() {
-  pkg-config cairo --cflags-only-I | grep -E "freetype2" && return 0
 
-  return 1;
+has_freetype() {
+  pkg-config cairo --cflags-only-I | grep freetype2
 }
 
-has_cairo_freetyep $1 > /dev/null
+has_freetype > /dev/null
+
 if test $? -eq 0; then
   echo true
 else
