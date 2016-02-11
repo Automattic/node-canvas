684731d3a1b35a0a489363faafc129a7a5aae24a
Caleb Hearon <chearon@moresteam.com>
2016-02-11 14:36:57 -0500

    implement Canvas::registerFont(ttf)
    this is implemented differently depending on the operating system. for
    MacOS, CTFontManagerRegisterFontsForURL is the call to add a temporary
    font. for Linux we use FontConfig, and for Windows use
    AddFontResourceEx. all have been tested successfully
    
    also updated examples/font.js

--- examples/font.js
+++ examples/font.js
@@ -4,42 +4,32 @@
  */
 
 var Canvas = require('../lib/canvas')
-  , canvas = new Canvas(320, 320)
-  , Font = Canvas.Font
-  , ctx = canvas.getContext('2d')
   , fs = require('fs')
   , path = require("path");
-
-if (!Font) {
-	throw new Error('Need to compile with font support');
-}
 
 function fontFile(name) {
   return path.join(__dirname, '/pfennigFont/', name);
 }
 
-var pfennigFont = new Font('pfennigFont', fontFile('Pfennig.ttf'));
-pfennigFont.addFace(fontFile('PfennigBold.ttf'),   'bold');
-pfennigFont.addFace(fontFile('PfennigItalic.ttf'), 'normal', 'italic');
-pfennigFont.addFace(fontFile('PfennigBoldItalic.ttf'), 'bold', 'italic');
+Canvas.registerFont(fontFile('Pfennig.ttf'));
+Canvas.registerFont(fontFile('PfennigBold.ttf'));
+Canvas.registerFont(fontFile('PfennigItalic.ttf'));
+Canvas.registerFont(fontFile('PfennigBoldItalic.ttf'));
 
-var canvas = new Canvas(320, 320)
-var ctx = canvas.getContext('2d')
-
-// Tell the ctx to use the font.
-ctx.addFont(pfennigFont);
+var canvas = new Canvas(320, 320);
+var ctx = canvas.getContext('2d');
 
 ctx.font = 'normal normal 50px Helvetica';
 
 ctx.fillText('Quo Vaids?', 0, 70);
 
-ctx.font = 'bold 50px pfennigFont';
+ctx.font = 'bold 50px Pfennig';
 ctx.fillText('Quo Vaids?', 0, 140);
 
-ctx.font = 'italic 50px pfennigFont';
+ctx.font = 'italic 50px Pfennig';
 ctx.fillText('Quo Vaids?', 0, 210);
 
-ctx.font = 'bold italic 50px pfennigFont';
+ctx.font = 'bold italic 50px Pfennig';
 ctx.fillText('Quo Vaids?', 0, 280);
 
 var out = fs.createWriteStream(__dirname + '/font.png');
