ec4a52b9c9f1782e97e601d682466e44619d1854
Caleb Hearon <chearon@moresteam.com>
2016-04-24 15:42:06 -0400

    font example: demonstrate custom font naming

--- examples/font.js
+++ examples/font.js
@@ -10,10 +10,10 @@ function fontFile (name) {
 // `registerFont`. When you set `ctx.font`, refer to the styles and the family
 // name as it is embedded in the TTF. If you aren't sure, open the font in
 // FontForge and visit Element -> Font Information and copy the Family Name
-Canvas.registerFont(fontFile('Pfennig.ttf'))
-Canvas.registerFont(fontFile('PfennigBold.ttf'))
-Canvas.registerFont(fontFile('PfennigItalic.ttf'))
-Canvas.registerFont(fontFile('PfennigBoldItalic.ttf'))
+Canvas.registerFont(fontFile('Pfennig.ttf'), {family: 'pfennigFont'})
+Canvas.registerFont(fontFile('PfennigBold.ttf'), {family: 'pfennigFont'})
+Canvas.registerFont(fontFile('PfennigItalic.ttf'), {family: 'pfennigFont'})
+Canvas.registerFont(fontFile('PfennigBoldItalic.ttf'), {family: 'pfennigFont'})
 
 var canvas = new Canvas(320, 320)
 var ctx = canvas.getContext('2d')
@@ -22,13 +22,13 @@ ctx.font = 'normal normal 50px Helvetica'
 
 ctx.fillText('Quo Vaids?', 0, 70)
 
-ctx.font = 'bold 50px Pfennig'
+ctx.font = 'bold 50px pfennigFont'
 ctx.fillText('Quo Vaids?', 0, 140)
 
-ctx.font = 'italic 50px Pfennig'
+ctx.font = 'italic 50px pfennigFont'
 ctx.fillText('Quo Vaids?', 0, 210)
 
-ctx.font = 'bold italic 50px Pfennig'
+ctx.font = 'bold italic 50px pfennigFont'
 ctx.fillText('Quo Vaids?', 0, 280)
 
 canvas.createPNGStream().pipe(fs.createWriteStream(path.join(__dirname, 'font.png')))
