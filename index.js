0bf10cb1c1dd044c99fe0b3f334b85c801af1e0e
Linus Unnebäck <linus@folkdatorn.se>
2017-08-29 21:36:17 +0200

    Lint browser.js and index.js with standard

--- index.js
+++ index.js
@@ -9,15 +9,15 @@ const PNGStream = require('./lib/pngstream')
 const PDFStream = require('./lib/pdfstream')
 const JPEGStream = require('./lib/jpegstream')
 
-function createCanvas(width, height, type) {
+function createCanvas (width, height, type) {
   return new Canvas(width, height, type)
 }
 
-function createImageData(array, width, height) {
+function createImageData (array, width, height) {
   return new bindings.ImageData(array, width, height)
 }
 
-function loadImage(src) {
+function loadImage (src) {
   return new Promise((resolve, reject) => {
     const image = new Image()
 
@@ -40,7 +40,7 @@ function loadImage(src) {
  * @param fontFace {{family: string, weight?: string, style?: string}} Object
  * specifying font information. `weight` and `style` default to `"normal"`.
  */
-function registerFont(src, fontFace){
+function registerFont (src, fontFace) {
   // TODO this doesn't need to be on Canvas; it should just be a static method
   // of `bindings`.
   return Canvas._registerFont(fs.realpathSync(src), fontFace)
@@ -73,8 +73,7 @@ module.exports = {
   /** jpeglib version. */
   jpegVersion: bindings.jpegVersion,
   /** gif_lib version. */
-  gifVersion: bindings.gifVersion ?
-    bindings.gifVersion.replace(/[^.\d]/g, '') : undefined,
+  gifVersion: bindings.gifVersion ? bindings.gifVersion.replace(/[^.\d]/g, '') : undefined,
   /** freetype version. */
-  freetypeVersion: bindings.freetypeVersion,
+  freetypeVersion: bindings.freetypeVersion
 }
