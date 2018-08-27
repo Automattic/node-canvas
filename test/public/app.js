6e155e27253c9df28a29701aff9eb308480fbbcf
Andrea Bogazzi <andreabogazzi79@gmail.com>
2018-08-28 01:08:25 +0200

    removed extra files

--- test/public/app.js
+++ test/public/app.js
@@ -18,7 +18,7 @@ function pdfLink (name) {
   })
 }
 
-function localRendering (name, callback) {
+function localRendering (name) {
   var canvas = create('canvas', { width: 200, height: 200, title: name })
 
   var ctx = canvas.getContext('2d', {alpha: true})
@@ -26,23 +26,9 @@ function localRendering (name, callback) {
   ctx.fillStyle = 'white'
   ctx.fillRect(0, 0, 200, 200)
   ctx.fillStyle = initialFillStyle
-  window.tests[name](ctx, callback)
-  return canvas
-}
+  window.tests[name](ctx, function () {})
 
-function getDifference (canvas, image, outputCanvas) {
-  var imgCanvas = create('canvas', { width: 200, height: 200 })
-  var ctx = imgCanvas.getContext('2d', {alpha: true})
-  var output = outputCanvas.getContext('2d', {alpha: true}).getImageData(0, 0, 200, 200)
-  ctx.drawImage(image, 0, 0, 200, 200)
-  var imageDataCanvas = ctx.getImageData(0, 0, 200, 200).data
-  var imageDataGolden = canvas.getContext('2d', {alpha: true}).getImageData(0, 0, 200, 200).data
-  window.pixelmatch(imageDataCanvas, imageDataGolden, output.data, 200, 200, {
-    includeAA: false,
-    threshold: 0.15
-  })
-  outputCanvas.getContext('2d', {alpha: true}).putImageData(output, 0, 0)
-  return outputCanvas
+  return canvas
 }
 
 function clearTests () {
@@ -59,22 +45,12 @@ function runTests () {
     create('thead', {}, [
       create('th', { textContent: 'node-canvas' }),
       create('th', { textContent: 'browser canvas' }),
-      create('th', { textContent: 'visual diffs' }),
       create('th', { textContent: '' })
     ]),
     create('tbody', {}, testNames.map(function (name) {
-      var img = create('img')
-      var canvasOuput = create('canvas', { width: 200, height: 200, title: name })
-      var canvas = localRendering(name, function () {
-        img.onload = function () {
-          getDifference(canvas, img, canvasOuput)
-        }
-        img.src = '/render?name=' + encodeURIComponent(name)
-      })
       return create('tr', {}, [
-        create('td', {}, [img]),
-        create('td', {}, [canvas]),
-        create('td', {}, [canvasOuput]),
+        create('td', {}, [create('img', { src: '/render?name=' + encodeURIComponent(name) })]),
+        create('td', {}, [localRendering(name)]),
         create('td', {}, [create('h3', { textContent: name }), pdfLink(name)])
       ])
     }))
