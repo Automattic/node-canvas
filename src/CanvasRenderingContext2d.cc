9c299074b842e5e6bdd9ac926e4b45b12897fc4a
Andrea Bogazzi <andreabogazzi79@gmail.com>
2018-09-03 07:50:18 +0200

    remove save restore

--- src/CanvasRenderingContext2d.cc
+++ src/CanvasRenderingContext2d.cc
@@ -1191,13 +1191,11 @@ NAN_METHOD(Context2d::DrawImage) {
   if (needsExtraSurface) {
     surfTemp = cairo_image_surface_create(CAIRO_FORMAT_ARGB32, dw, dh);
     ctxTemp = cairo_create(surfTemp);
-    cairo_save(ctxTemp);
     cairo_scale(ctxTemp, fx, fy);
     cairo_set_source_surface(ctxTemp, surface, -sx, -sy);
     cairo_pattern_set_filter(cairo_get_source(ctxTemp), context->state->patternQuality);
     cairo_pattern_set_extend(cairo_get_source(ctxTemp), CAIRO_EXTEND_REFLECT);
     cairo_paint_with_alpha(ctxTemp, 1);
-    cairo_restore(ctxTemp);
     surface = surfTemp;
   }
 
