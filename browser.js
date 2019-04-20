4dc261f4451840ed075d2462230d6bffb6c8099d
tcme <546900+thisconnect@users.noreply.github.com>
2019-04-20 09:28:19 +0200

    support any properties on an HTMLImageElement
    https://github.com/Automattic/node-canvas/pull/1402#issuecomment-484494249

--- browser.js
+++ browser.js
@@ -18,10 +18,9 @@ exports.createImageData = function (array, width, height) {
   }
 }
 
-exports.loadImage = function (src) {
+exports.loadImage = function (src, options) {
   return new Promise((resolve, reject) => {
-    const image = document.createElement('img')
-    image.crossOrigin = 'anonymous'
+    const image = Object.assign(document.createElement('img'), options)
 
     function cleanup () {
       image.onload = null
