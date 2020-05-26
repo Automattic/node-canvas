5b599a5080d19684bb32a092e3a2d90f76810f3e
Sakari Lehtonen <sakari@psitriangle.net>
2020-05-26 13:19:22 +0300

    Move getSafeBufSize() under #ifdef HAVE_JPEG, removes compiler warning of unused function removal

--- src/Canvas.cc
+++ src/Canvas.cc
@@ -314,12 +314,6 @@ static void parseJPEGArgs(Local<Value> arg, JpegClosure& jpegargs) {
 }
 #endif
 
-static uint32_t getSafeBufSize(Canvas* canvas) {
-  // Don't allow the buffer size to exceed the size of the canvas (#674)
-  // TODO not sure if this is really correct, but it fixed #674
-  return (std::min)(canvas->getWidth() * canvas->getHeight() * 4, static_cast<int>(PAGE_SIZE));
-}
-
 #if CAIRO_VERSION >= CAIRO_VERSION_ENCODE(1, 16, 0)
 
 static inline void setPdfMetaStr(cairo_surface_t* surf, Local<Object> opts,
@@ -679,6 +673,11 @@ NAN_METHOD(Canvas::StreamPDFSync) {
  */
 
 #ifdef HAVE_JPEG
+static uint32_t getSafeBufSize(Canvas* canvas) {
+  // Don't allow the buffer size to exceed the size of the canvas (#674)
+  // TODO not sure if this is really correct, but it fixed #674
+  return (std::min)(canvas->getWidth() * canvas->getHeight() * 4, static_cast<int>(PAGE_SIZE));
+}
 
 NAN_METHOD(Canvas::StreamJPEGSync) {
   if (!info[1]->IsFunction())
@@ -698,7 +697,6 @@ NAN_METHOD(Canvas::StreamJPEGSync) {
   }
   return;
 }
-
 #endif
 
 char *
