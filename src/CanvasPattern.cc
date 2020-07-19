4e963c276c0bf24e783055ed03c89e1564eb5b1d
Christian Swinehart <drafting@samizdat.cc>
2020-07-19 15:28:12 -0400

    add support for CanvasPattern.setTransform

--- src/CanvasPattern.cc
+++ src/CanvasPattern.cc
@@ -25,6 +25,7 @@ Pattern::Initialize(Nan::ADDON_REGISTER_FUNCTION_ARGS_TYPE target) {
   constructor.Reset(ctor);
   ctor->InstanceTemplate()->SetInternalFieldCount(1);
   ctor->SetClassName(Nan::New("CanvasPattern").ToLocalChecked());
+  Nan::SetPrototypeMethod(ctor, "setTransform", SetTransform);
 
   // Prototype
   Local<Context> ctx = Nan::GetCurrentContext();
@@ -81,6 +82,35 @@ NAN_METHOD(Pattern::New) {
   pattern->Wrap(info.This());
   info.GetReturnValue().Set(info.This());
 }
+
+/*
+ * Set the pattern-space to user-space transform.
+ */
+NAN_METHOD(Pattern::SetTransform) {
+  Pattern *pattern = Nan::ObjectWrap::Unwrap<Pattern>(info.This());
+  Local<Context> ctx = Nan::GetCurrentContext();
+  Local<Object> mat = Nan::To<Object>(info[0]).ToLocalChecked();
+
+#if NODE_MAJOR_VERSION >= 8
+  if (!mat->InstanceOf(ctx, _DOMMatrix.Get(Isolate::GetCurrent())).ToChecked()) {
+    return Nan::ThrowTypeError("Expected DOMMatrix");
+  }
+#endif
+
+  cairo_matrix_t matrix;
+  cairo_matrix_init(&matrix,
+    Nan::To<double>(Nan::Get(mat, Nan::New("a").ToLocalChecked()).ToLocalChecked()).FromMaybe(1),
+    Nan::To<double>(Nan::Get(mat, Nan::New("b").ToLocalChecked()).ToLocalChecked()).FromMaybe(0),
+    Nan::To<double>(Nan::Get(mat, Nan::New("c").ToLocalChecked()).ToLocalChecked()).FromMaybe(0),
+    Nan::To<double>(Nan::Get(mat, Nan::New("d").ToLocalChecked()).ToLocalChecked()).FromMaybe(1),
+    Nan::To<double>(Nan::Get(mat, Nan::New("e").ToLocalChecked()).ToLocalChecked()).FromMaybe(0),
+    Nan::To<double>(Nan::Get(mat, Nan::New("f").ToLocalChecked()).ToLocalChecked()).FromMaybe(0)
+  );
+
+  cairo_matrix_invert(&matrix);
+  cairo_pattern_set_matrix(pattern->_pattern, &matrix);
+}
+
 
 /*
  * Initialize pattern.
