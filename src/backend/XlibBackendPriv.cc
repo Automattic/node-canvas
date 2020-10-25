7db6bc6cee6752690dea0fafd1cfb1b7fa75cc40
Jesús Leganés-Combarro 'piranna <piranna@gmail.com>
2020-10-25 21:25:57 +0100

    Set window size hints

--- src/backend/XlibBackendPriv.cc
+++ src/backend/XlibBackendPriv.cc
@@ -1,6 +1,21 @@
 #include "XlibBackendPriv.h"
 
 #include <cairo-xlib.h>
+#include <X11/Xlibint.h>
+#include <X11/Xutil.h>
+
+
+void setSizeHints(Display* display, Window window, int width, int height)
+{
+	XSizeHints* sh = XAllocSizeHints();
+
+	sh->flags = PMinSize | PMaxSize;
+	sh->min_width = sh->max_width = width;
+	sh->min_height = sh->max_height = height;
+
+	XSetWMNormalHints(display, window, sh);
+	XFree(sh);
+}
 
 
 XlibBackendPriv::XlibBackendPriv(int width, int height, char *display_name)
@@ -12,6 +27,8 @@ XlibBackendPriv::XlibBackendPriv(int width, int height, char *display_name)
 
 	this->window = XCreateSimpleWindow(display, RootWindow(display, 0), 0, 0,
     width, height, 0, 0, BlackPixel(display, 0));
+
+	setSizeHints(this->display, this->window, width, height);
 
 	XSelectInput(this->display, this->window, StructureNotifyMask | ExposureMask);
 	XMapWindow(this->display, this->window);
@@ -32,5 +49,7 @@ cairo_surface_t* XlibBackendPriv::cairo_surface_create(int width, int height)
 
 void XlibBackendPriv::ResizeWindow(int width, int height)
 {
+	setSizeHints(this->display, this->window, width, height);
+
 	XResizeWindow(this->display, this->window, width, height);
 }
