30c41e17df96d182a79660d46fc967292f5c65c9
Jesús Leganés-Combarro 'piranna <piranna@gmail.com>
2020-10-25 21:26:45 +0100

    Remove window Close and Maximize buttons and resize operation

--- src/backend/XlibBackendPriv.cc
+++ src/backend/XlibBackendPriv.cc
@@ -1,9 +1,48 @@
 #include "XlibBackendPriv.h"
 
 #include <cairo-xlib.h>
+#include <X11/Xatom.h>
 #include <X11/Xlibint.h>
 #include <X11/Xutil.h>
 
+
+// Extracted from Xm/MwmUtil.h
+struct MwmHints {
+    unsigned long flags;
+    unsigned long functions;
+    unsigned long decorations;
+    long          input_mode;
+    unsigned long status;
+};
+enum {
+    MWM_HINTS_FUNCTIONS   = (1L << 0),
+    MWM_HINTS_DECORATIONS = (1L << 1),
+
+    MWM_FUNC_ALL      = (1L << 0),
+    MWM_FUNC_RESIZE   = (1L << 1),
+    MWM_FUNC_MOVE     = (1L << 2),
+    MWM_FUNC_MINIMIZE = (1L << 3),
+    MWM_FUNC_MAXIMIZE = (1L << 4),
+    MWM_FUNC_CLOSE    = (1L << 5)
+};
+
+
+// We control the window from code, so we try to prevent windows manager to
+// close or resize the window. Doing so correctly, would requires us to capture
+// the window manager events in a thread loop and update canvas size, but it's
+// not clear if canvas size should change or being scaled/zoomed or if it should
+// maintain aspect ratio.
+
+void configWindow(Display* display, Window window)
+{
+	struct MwmHints hints;
+	hints.flags = MWM_HINTS_FUNCTIONS;
+	hints.functions = MWM_FUNC_MOVE | MWM_FUNC_MINIMIZE;
+
+	Atom wm = XInternAtom(display, "_MOTIF_WM_HINTS", False);
+	XChangeProperty(display, window, wm, XA_ATOM, 32, PropModeReplace,
+		(unsigned char*)&hints, 5);
+}
 
 void setSizeHints(Display* display, Window window, int width, int height)
 {
@@ -28,6 +67,7 @@ XlibBackendPriv::XlibBackendPriv(int width, int height, char *display_name)
 	this->window = XCreateSimpleWindow(display, RootWindow(display, 0), 0, 0,
     width, height, 0, 0, BlackPixel(display, 0));
 
+	configWindow(this->display, this->window);
 	setSizeHints(this->display, this->window, width, height);
 
 	XSelectInput(this->display, this->window, StructureNotifyMask | ExposureMask);
