eaaf9b8bc190bbaf2a59df046b68db22effd3a33
Nick Harrison <42382362+nickpharrison@users.noreply.github.com>
2021-01-19 10:51:44 +0800

    Switch to using global namespace-defined toupper
    std::toupper is not defined, so use global version instead

--- src/Util.h
+++ src/Util.h
@@ -28,6 +28,6 @@ inline void SetProtoAccessor(
 
 inline bool streq_casein(std::string& str1, std::string& str2) {
   return str1.size() == str2.size() && std::equal(str1.begin(), str1.end(), str2.begin(), [](char& c1, char& c2) {
-    return c1 == c2 || std::toupper(c1) == std::toupper(c2);
+    return c1 == c2 || ::toupper(c1) == ::toupper(c2);
   });
 }
