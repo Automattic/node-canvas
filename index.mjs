ba0e4ff97b02f44d4a8a645dcc566fc88d0c6a4f
A. Román <kyradiscord@gmail.com>
2022-06-02 21:03:12 +0200

    refactor: destructure once
    Co-authored-by: Mohammed Keyvanzadeh <mohammadkeyvanzade94@gmail.com>

--- index.mjs
+++ index.mjs
@@ -1,28 +1,28 @@
 import mod from './index.js'
 
 export default mod;
-export const { Canvas } = mod;
-export const { Context2d } = mod;
-export const { CanvasRenderingContext2D } = mod;
-export const { CanvasGradient } = mod;
-export const { CanvasPattern } = mod;
-export const { Image } = mod;
-export const { ImageData } = mod;
-export const { PNGStream } = mod;
-export const { PDFStream } = mod;
-export const { JPEGStream } = mod;
-export const { DOMMatrix } = mod;
-export const { DOMPoint } = mod;
-export const { registerFont } = mod;
-export const { deregisterAllFonts } = mod;
-export const { parseFont } = mod;
-export const { createCanvas } = mod;
-export const { createImageData } = mod;
-export const { loadImage } = mod;
-export const { backends } = mod;
-export const { version } = mod;
-export const { cairoVersion } = mod;
-export const { jpegVersion } = mod;
-export const { gifVersion } = mod;
-export const { freetypeVersion } = mod;
-export const { rsvgVersion } = mod;
+export const {
+  Canvas,
+  Context2d,
+  CanvasRenderingContext2D,
+  CanvasGradient,
+  CanvasPattern,
+  Image,
+  ImageData,
+  PNGStream,
+  DOMMatrix,
+  DOMPoint,
+  registerFont,
+  deregisterAllFonts,
+  parseFont,
+  createCanvas,
+  createImageData,
+  loadImage,
+  backends,
+  version,
+  cairoVersion,
+  jpegVersion,
+  gifVersion,
+  freetypeVersion,
+  rsvgVersion
+} = mod;
