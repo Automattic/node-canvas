ad18c6ce0ab2452d64df36a199c740a926ceb939
Zach Bjornson <zbbjornson@gmail.com>
2022-10-29 18:24:19 -0700

    src: shorten copy assignment operator decl for Point

--- src/Point.h
+++ src/Point.h
@@ -1,17 +1,11 @@
 // Copyright (c) 2010 LearnBoost <tj@learnboost.com>
 #pragma once
 
-#include <algorithm>
-
 template <typename T>
 class Point {
   public:
     T x, y;
     Point(T x=0, T y=0): x(x), y(y) {}
     Point(const Point&) = default;
-    Point& operator=(Point other) {
-      std::swap(x, other.x);
-      std::swap(y, other.y);
-      return *this;
-    }
+    Point& operator=(const Point&) = default;
 };
