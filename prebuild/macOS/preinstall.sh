7f152cc8155b8b243e7ebb4e89b2a690fdb18a59
Zach Bjornson <zbbjornson@gmail.com>
2022-12-22 10:47:58 -0800

    gdmacos

--- prebuild/macOS/preinstall.sh
+++ prebuild/macOS/preinstall.sh
@@ -1,4 +1,5 @@
 brew update
+rm /usr/local/bin/2to3
 brew install pkg-config cairo pango librsvg giflib # python3 is for macpack
 # brew upgrade python # activates python 3
 pip3 install --user macpack
