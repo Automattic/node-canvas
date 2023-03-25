6fc0c5f74b82e95ec1670a9b402db1666a3eb8e8
Caleb Hearon <caleb@chearon.net>
2023-03-25 13:39:04 -0400

    fix not compiling on certain windows versions

--- src/register_font.cc
+++ src/register_font.cc
@@ -264,7 +264,7 @@ get_pango_font_description(unsigned char* filepath) {
         FILE_SHARE_READ,
         NULL,
         OPEN_EXISTING,
-        NULL,
+        FILE_ATTRIBUTE_NORMAL,
         NULL
   );
   if(!hFile){
