9d03fcd2b3e7743698de1d34293c5507399626d3
Jacob Sapoznikow <jacob1coder@gmail.com>
2023-06-23 22:33:54 +0000

    Fix PAX

--- prebuild/Linux/preinstall.sh
+++ prebuild/Linux/preinstall.sh
@@ -1,7 +1,7 @@
 # apt-get-style dependencies aren't done here since the
 # linux build is done on a docker image that has them
 
-git clone git://anongit.gentoo.org/proj/pax-utils.git
+git clone git://anongit.gentoo.org/proj/pax-utils.git -b v1.2.9
 cd pax-utils
 PATH=$PATH:$PWD
 make
