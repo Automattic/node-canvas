b25f486712dade65322b6deb2ba8c9083a8b030c
Jacob Sapoznikow <jacob1coder@gmail.com>
2023-06-24 00:56:31 +0000

    [ci skip] Update scripts

--- prebuild/Linux/bundle.sh
+++ prebuild/Linux/bundle.sh
@@ -1,4 +1,4 @@
-copies=$(lddtree.sh -l build/Release/canvas.node | sed -r -e '/^\/lib/d' -e '/canvas.node$/d');
+copies=$(lddtree -l build/Release/canvas.node | sed -r -e '/^\/lib/d' -e '/canvas.node$/d');
 
 for so in $copies; do
   cp $so build/Release
