4ccab4ffafe0badc801461b852a261d16bce5384
Jacob Sapoznikow <jacob1coder@gmail.com>
2023-06-24 01:41:00 +0000

    Fix detect-libc

--- prebuild/tarball.sh
+++ prebuild/tarball.sh
@@ -1,10 +1,14 @@
 # Generate the node-gyp formatted filename from the node environment
-FILENAME=$(node -e " \
-    var p = process, v = p.versions, libc = require('detect-libc').family || 'unknown'; \
-    const tagName = p.env.UPLOAD_TO || p.env.CANVAS_VERSION_TO_BUILD; \
-    const arch = p.env.CANVAS_ARCH_TO_BUILD || p.arch; \
-    console.log(['canvas', tagName, 'node-v' + v.modules, p.platform, libc, arch].join('-')); \
-").tar.gz;
+npm install --global detect-libc
+
+FILENAME=$(
+  node -e "
+    var p = process, v = p.versions, libc = require('detect-libc').familySync() || 'unknown';
+    const tagName = p.env.UPLOAD_TO || p.env.CANVAS_VERSION_TO_BUILD;
+    const arch = p.env.CANVAS_ARCH_TO_BUILD || p.arch;
+    console.log(['canvas', tagName, 'node-v' + v.modules, p.platform, libc, arch].join('-'));
+  "
+).tar.gz;
 
 # Zip up the release
 tar -C build -czvf $FILENAME Release
