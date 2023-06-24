bbccada9feeedd4a947bbd4086682b6990efa57c
Jacob Sapoznikow <jacob1coder@gmail.com>
2023-06-24 01:57:59 +0000

    Fix node gyp

--- prebuild/Linux/gyp.sh
+++ prebuild/Linux/gyp.sh
@@ -5,4 +5,14 @@ NPM_BASE_PATH="$(dirname "$(dirname "$NPM_EXEC_PATH")")"
 NODE_GYP_PACKAGE_JSON="$NPM_EXEC_PATH/node_modules/node-gyp/package.json"
 NODE_GYP_VERSION="$(cat "$NODE_GYP_PACKAGE_JSON" | jq -r ".version")"
 
-npm install -g "node-gyp@$NODE_GYP_VERSION"
+if [[ "$(node -v)" == "v13"* ]] || \
+   [[ "$(node -v)" == "v12"* ]] || \
+   [[ "$(node -v)" == "v11"* ]] || \
+   [[ "$(node -v)" == "v10"* ]]; \
+then
+    npm install -g node-gyp@8.4.1
+elif [[ "$(node -v)" == "v9"* ]] || [[ "$(node -v)" == "v8"* ]]; then
+    npm install -g node-gyp@7.1.2
+else
+    npm install -g "node-gyp@$NODE_GYP_VERSION"
+fi
