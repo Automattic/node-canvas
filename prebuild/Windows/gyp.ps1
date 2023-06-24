4fa140e33abd1ecfa0ec6b356bafaa578acdaf1a
Jacob Sapoznikow <jacob1coder@gmail.com>
2023-06-24 05:24:55 +0000

    [ci skip] Fixing gyp in windows

---
+++ prebuild/Windows/gyp.ps1
@@ -0,0 +1,12 @@
+$NPMExecPath = (Resolve-Path (Get-Command npm).Source).Path
+$NPMBasePath = (Resolve-Path "$(Get-Item $NPMExecPath)/../..").Path
+$NodeGYPPackageJson = Join-Path $NPMBasePath "lib\node_modules\node-gyp\package.json"
+$NodeGYPVersion = (Get-Content $NodeGYPPackageJson | ConvertFrom-Json).version
+
+if (("$(node -v)" -like "v13*") -or ("$(node -v)" -like "v12*") -or ("$(node -v)" -like "v11*") -or ("$(node -v)" -like "v10*")) {
+    npm install -g node-gyp@8.4.1
+} elseif (("$(node -v)" -like "v9*") -or ("$(node -v)" -like "v8*")) {
+    npm install -g node-gyp@6.1.0
+} else {
+    npm install -g "node-gyp@$NodeGYPVersion"
+}
