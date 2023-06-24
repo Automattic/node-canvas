4897c4e930e516622707b94b2ae4cf6bc27ad527
Jacob Sapoznikow <jacob1coder@gmail.com>
2023-06-24 03:17:34 +0000

    [ci skip] Fix macos preinstall

--- prebuild/macOS/preinstall.sh
+++ prebuild/macOS/preinstall.sh
@@ -1,4 +1,4 @@
 brew update
 brew install pkg-config cairo pango librsvg python3 giflib # python3 is for macpack
-brew upgrade python # activates python 3
+brew install python@3.9 # activates python 3
 pip3 install --user macpack
