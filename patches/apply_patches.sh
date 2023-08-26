42f2ae68ac4bb0eafc528c513eb175c57d03913a
Jacob Sapoznikow <jacob1coder@gmail.com>
2023-08-26 18:55:49 +0000

    Patch v8

--- patches/apply_patches.sh
+++ patches/apply_patches.sh
@@ -13,6 +13,6 @@ cd /tmp/zig
 patch -p0 "$DIR/patches/zig.patch"
 
 # Patch v8
-cd "$DIR"
-sed -i 's/static_cast\<int\>\(reinterpret_cast\<intptr_t\>\(value\)\)/static_cast<int>(reinterpret_cast<intptr_t>((void*) value))/g' ~/.cache/node-gyp/*/include/node/v8.h
-
+node-gyp install
+cd ~/.cache/node-gyp
+patch -p0 "$DIR/patches/v8.patch"
