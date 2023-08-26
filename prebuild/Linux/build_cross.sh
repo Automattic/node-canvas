b8d067182a94d96adb0387c6086390d749141766
Jacob Sapoznikow <jacob1coder@gmail.com>
2023-08-26 17:43:39 +0000

    Target

--- prebuild/Linux/build_cross.sh
+++ prebuild/Linux/build_cross.sh
@@ -11,8 +11,8 @@ if [ "$ARCH" = "arm64" ]; then
     tar -xJvf /tmp/zig.txz -C /tmp/zig
     ZIG="/tmp/zig/zig-linux-x86_64-0.12.0-dev.168+a31748b29/zig"
 
-    export CC="$ZIG cc"
-    export CXX="$ZIG c++"
+    export CC="$ZIG cc -target aarch64-linux-gnu"
+    export CXX="$ZIG c++ -target aarch64-linux-gnu"
     export AR="$ZIG ar"
     export AS="aarch64-linux-gnu-as"
     export LD="aarch64-linux-gnu-ld"
