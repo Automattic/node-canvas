3160662a9654d4ae272d68431647babfaa709f4b
Jacob Sapoznikow <jacob1coder@gmail.com>
2023-08-26 05:25:50 +0000

    arm toolchain?

--- prebuild/Linux/build_cross.sh
+++ prebuild/Linux/build_cross.sh
@@ -3,12 +3,20 @@
 set -ex
 
 if [ "$ARCH" = "arm64" ]; then
-    export CC=aarch64-linux-gnu-gcc
-    export CXX=aarch64-linux-gnu-g++
-    export AR=aarch64-linux-gnu-ar
-    export AS=aarch64-linux-gnu-as
-    export LD=aarch64-linux-gnu-ld
-    export RANLIB=aarch64-linux-gnu-ranlib
+    curl -L \
+        https://developer.arm.com/-/media/Files/downloads/gnu/12.3.rel1/binrel/arm-gnu-toolchain-12.3.rel1-x86_64-aarch64-none-linux-gnu.tar.xz \
+        -o arm.txz
+    
+    mkdir arm-toolchain
+    tar -xJvf arm.txz -C arm-toolchain
+    BIN_DIR="$(pwd)/arm-toolchain/arm-gnu-toolchain-12.3.rel1-x86_64-aarch64-none-linux-gnu/bin/"
+
+    export CC="$BIN_DIR/aarch64-none-linux-gnu-gcc"
+    export CXX="$BIN_DIR/aarch64-none-linux-gnu-g++"
+    export AR="$BIN_DIR/aarch64-none-linux-gnu-ar"
+    export AS="$BIN_DIR/aarch64-none-linux-gnu-as"
+    export LD="$BIN_DIR/aarch64-none-linux-gnu-ld"
+    export RANLIB="$BIN_DIR/aarch64-none-linux-gnu-ranlib"
 else
     export CC=x86_64-linux-gnu-gcc
     export CXX=x86_64-linux-gnu-g++
@@ -17,10 +25,6 @@ else
     export LD=x86_64-linux-gnu-ld
     export RANLIB=x86_64-linux-gnu-ranlib
 fi
-
-export CFLAGS="--disable-multilib"
-export CXXFLAGS="--disable-multilib"
-export LDFLAGS="--disable-multilib"
 
 install-node-gyp
 npm install --ignore-scripts
