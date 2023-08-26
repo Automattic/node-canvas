9f2e7c3700c8b4ca1c21826b014c7995c9e537a7
Jacob Sapoznikow <jacob1coder@gmail.com>
2023-08-26 18:58:42 +0000

    Fix patch script

--- patches/apply_patches.sh
+++ patches/apply_patches.sh
@@ -6,13 +6,13 @@ DIR="$(pwd)"
 
 # Patch nan
 cd node_modules
-patch -p0 "$DIR/patches/nan_callbacks_12_inl.patch"
+patch -p0 -i "$DIR/patches/nan_callbacks_12_inl.patch"
 
 # Patch zig
 cd /tmp/zig
-patch -p0 "$DIR/patches/zig.patch"
+patch -p0 -i "$DIR/patches/zig.patch"
 
 # Patch v8
 node-gyp install
-cd ~/.cache/node-gyp
-patch -p0 "$DIR/patches/v8.patch"
+cd ~/.cache
+patch -p0 -i -t "$DIR/patches/v8.patch" || true
