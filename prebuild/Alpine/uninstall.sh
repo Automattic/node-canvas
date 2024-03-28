1c328a77a742ef7acc99c8889e33f2f98369cd9f
Sergey Vohmyanin <delagen@gmail.com>
2024-03-28 10:20:17 +0300

    feat(prebuild): support of Alpine binaries
    - update dependencies
    - rework binding.gyp
    - rework msys bundle for Windows
    - add bundle for MUSL (Alpine)
    - rework bundle scripts and ci
    
    moved to prebuild

---
+++ prebuild/Alpine/uninstall.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env sh
+
+apk --purge del build-base cairo* jpeg* pango* giflib* librsvg* pixman*
--- prebuild/macOS/preinstall.sh
+++ prebuild/macOS/preinstall.sh
@@ -1,13 +1,2 @@
-set -Eeuxo pipefail
-
-brew update
-
-rm -f /usr/local/bin/2to3 || :
-rm -f /usr/local/bin/idle3 || :
-rm -f /usr/local/bin/pydoc3 || :
-rm -f /usr/local/bin/python3 || :
-rm -f /usr/local/bin/python3-config || :
-
-brew install --force pkg-config cairo pango librsvg giflib
-
-pip3 install --user macpack
+#!/usr/bin/env sh
+brew install pkg-config cairo pango libpng giflib librsvg pixman
