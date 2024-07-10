c41b7369c66bff61416b47a785bfe91dedff3fea
James Vaughan <james@jamesbvaughan.com>
2024-07-09 18:17:12 -0700

    Restore changes to tarball.sh

--- prebuild/tarball.sh
+++ prebuild/tarball.sh
@@ -6,14 +6,14 @@ FILENAME=$(
     const tagName = p.env.UPLOAD_TO || p.env.CANVAS_VERSION_TO_BUILD;
     console.log('canvas-' + tagName + '-napi-v7-' + p.platform + libc + '-' + p.arch);
   "
-).tar.gz
+).tar.gz;
 
 # Zip up the release
 tar -czvf $FILENAME build
 
 if [ $? -ne 0 ]; then
   echo "failed to make tarball $FILENAME from node-canvas/build"
-  exit 1
+  exit 1;
 else
   echo "::set-output name=asset_name::$FILENAME"
 fi
