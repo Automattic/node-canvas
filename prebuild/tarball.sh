fa6374f3c30a60849e34159da57b8e370b352b12
Simon Hailes <simon@yellaumbrella.tv>
2024-11-01 16:39:41 +0000

    fix uploaded names

--- prebuild/tarball.sh
+++ prebuild/tarball.sh
@@ -1,8 +1,11 @@
 # Generate the prebuild-install formatted filename from the node environment
+# match prebuild-install style.
 FILENAME=$(
   node -e "
-    var p = process, v = p.versions, libc = require('detect-libc').familySync() || 'unknown';
-    if (libc === 'glibc') libc = '';
+    var p = process, v = p.versions;
+    const detectLibc = require('detect-libc');
+    const libc = process.env.LIBC || process.env.npm_config_libc ||
+      (detectLibc.isNonGlibcLinuxSync() && detectLibc.familySync()) || ''
     const tagName = p.env.UPLOAD_TO || p.env.CANVAS_VERSION_TO_BUILD;
     console.log('canvas-' + tagName + '-napi-v7-' + p.platform + libc + '-' + p.arch);
   "
